a76ccb21e0e64b8aeea9dc0b832e9c2f862206a4
Wilfredo Domanico <wilfredo.domanico.jr@gmail.com>
2026-04-23 21:27:07 +0800

    First Commit

---
+++ resources/views/components/app-logo-icon.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('images/app-logo.png') }}" alt="Logo" {{ $attributes }} />
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Livewire\Volt\Volt;
+
+Route::redirect('/', '/login');
+
+Route::view('home', 'home')
+    ->middleware(['auth', 'verified'])
+    ->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::redirect('settings', 'settings/profile');
+
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+});
+
+require __DIR__ . '/auth.php';
